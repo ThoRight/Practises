8460c873b23e49d245ef3aeec10a3726151fc163
Murat E <muraterbiliciofficial@gmail.com>
2024-08-02 12:43:42 +0300

    improving interface

--- CrudApp/index.php
+++ CrudApp/index.php
@@ -9,15 +9,24 @@ include('validation.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Main Page</title>
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
     <script src="jquery3.7.1.js"></script>
     <script src="script.js"></script>
 </head>
 
 <body>
+    <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="index.php" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="create.php" class="nav-link">Create User</a></li>
+            <li class="nav-item"><a href="update.php" class="nav-link">Update User</a></li>
+            <li class="nav-item"><a href="delete.php" class="nav-link">Delete User</a></li>
+            <li class="nav-item"><a href="read.php" class="nav-link">Display User</a></li>
+        </ul>
+    </header>
     <form action="create.php" method="get">
-        <input type="submit" value="Create User">
     </form>
     <?php
     $status = "";
@@ -25,21 +34,28 @@ include('validation.php');
         $status = $_GET['status'];
         if ($status === 'success_create') :
     ?>
-            <p>Created Account Successfully!</p>
+            <div class="alert alert-success font-weight-bold" role="alert">
+                Account has been created.
+            </div>
         <?php
         elseif ($status === 'success_update') :
         ?>
-            <p>Updated Account Successfully!</p>
+            <div class="alert alert-success font-weight-bold" role="alert">
+                Account has been updated.
+            </div>
         <?php
         elseif ($status === 'success_delete') :
         ?>
-            <p>Deleted Account Successfully!</p>
-
+            <div class="alert alert-success font-weight-bold" role="alert">
+                Account has been deleted.
+            </div>
         <?php
         elseif ($status === 'error') :
         ?>
             <?php $errorStr = isset($_GET['errorStr']) ? htmlspecialchars(urldecode($_GET['errorStr']), ENT_QUOTES, 'UTF-8') : 'gg'; ?>
-            <p>Error!! <?php echo $errorStr ?></p>
+            <div class="alert alert-danger font-weight-bold" role="alert">
+                Error!! <?php echo $errorStr ?>
+            </div>
         <?php
         elseif ($status === '') :
         ?>
@@ -47,7 +63,7 @@ include('validation.php');
         <?php
         else :
         ?>
-            <p>Unknown status: <?php echo htmlspecialchars($status); ?></p>
+            <p class="font-weight-bold">Unknown status: <?php echo htmlspecialchars($status); ?></p>
         <?php
         endif;
     else :
@@ -57,15 +73,14 @@ include('validation.php');
     endif;
     ?>
     <form action="read.php" method="get">
-        <input type="submit" value="Display Users">
     </form>
     <form action="update.php" method="get">
-        <input type="submit" value="Update Users">
     </form>
     <form action="delete.php" method="get">
-        <input type="submit" value="Delete Users">
     </form>
-
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- CrudApp/read.php
+++ CrudApp/read.php
@@ -24,52 +24,100 @@ $result = $conn->query($sql);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Read Users</title>
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <table>
-        <tr>
-            <th>Id</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Password</th>
-        </tr>
-        <?php
-        if ($result->num_rows > 0) :
-            while ($row = $result->fetch_object()) :
-        ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($row->id); ?></td>
-                    <td><?php echo htmlspecialchars($row->username); ?></td>
-                    <td><?php echo htmlspecialchars($row->email); ?></td>
-                    <td><?php echo htmlspecialchars($row->password); ?></td>
-                </tr>
-        <?php
-            endwhile;
-        endif;
-        ?>
+    <div class="container">
+        <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="index.php" class="nav-link" aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="create.php" class="nav-link">Create User</a></li>
+                <li class="nav-item"><a href="update.php" class="nav-link">Update User</a></li>
+                <li class="nav-item"><a href="delete.php" class="nav-link">Delete User</a></li>
+                <li class="nav-item"><a href="read.php" class="nav-link active">Display User</a></li>
+            </ul>
+        </header>
+    </div>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Id</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+                <th scope="col">Password</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $currentRowNum = $start_from + 1; // Start numbering from the correct offset
+            if ($result->num_rows > 0) :
+                while ($row = $result->fetch_object()) :
+            ?>
+                    <tr>
+                        <th scope="row"><?php echo $currentRowNum; ?></th>
+                        <td><?php echo htmlspecialchars($row->id); ?></td>
+                        <td><?php echo htmlspecialchars($row->username); ?></td>
+                        <td><?php echo htmlspecialchars($row->email); ?></td>
+                        <td><?php echo htmlspecialchars($row->password); ?></td>
+                    </tr>
+                    <?php $currentRowNum++; ?>
+            <?php
+                endwhile;
+            endif;
+            ?>
+        </tbody>
+    </table>
 
-        <div class="pagination">
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
             <?php if ($page > 1) : ?>
-                <a href="read.php?page=<?php echo $page - 1; ?>">Previous</a>
+                <li class="page-item">
+                    <a class="page-link" href="read.php?page=<?php echo $page - 1; ?>" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+            <?php else : ?>
+                <li class="page-item disabled">
+                    <span class="page-link" aria-label="Previous">&laquo;</span>
+                </li>
             <?php endif; ?>
 
-            <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
-                <a href="read.php?page=<?php echo $i; ?>">
-                    <?php echo $i; ?>
-                </a>
-            <?php endfor; ?>
+            <?php if ($page > 1) : ?>
+                <li class="page-item">
+                    <a class="page-link" href="read.php?page=<?php echo $page - 1; ?>"><?php echo $page - 1; ?></a>
+                </li>
+            <?php endif; ?>
+
+            <li class="page-item active" aria-current="page">
+                <span class="page-link"><?php echo $page; ?></span>
+            </li>
 
             <?php if ($page < $total_pages) : ?>
-                <a href="read.php?page=<?php echo $page + 1; ?>">Next</a>
+                <li class="page-item">
+                    <a class="page-link" href="read.php?page=<?php echo $page + 1; ?>"><?php echo $page + 1; ?></a>
+                </li>
             <?php endif; ?>
-        </div>
-    </table>
-    <form action="index.php">
-        <input type="submit" value="Main Menu">
-    </form>
+
+            <?php if ($page < $total_pages) : ?>
+                <li class="page-item">
+                    <a class="page-link" href="read.php?page=<?php echo $page + 1; ?>" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            <?php else : ?>
+                <li class="page-item disabled">
+                    <span class="page-link" aria-label="Next">&raquo;</span>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
