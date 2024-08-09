4b5bca0990ab1bf860bba3e89da20ecf20c1a6a2
Murat E <muraterbiliciofficial@gmail.com>
2024-08-09 16:54:52 +0300

    database operations, pagination for comments, navbar

--- BlogAndCMS/test/test_add_post.php
+++ BlogAndCMS/test/test_add_post.php
@@ -1,4 +1,30 @@
-<?php
+<<!DOCTYPE html>
+    <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <script src="jquery3.7.1.js"></script>
+        <script src="test_add_post.js" async defer></script>
+    </body>
+
+    </html>
+    <?php
+/*
 // The URL of the PHP script that will handle the request
 $url = 'http://localhost/BlogAndCMS/api/add_post.php'; // Replace with the actual URL of your PHP script
 for ($num = 8; $num < 58; ++$num) {
@@ -33,3 +59,4 @@ for ($num = 8; $num < 58; ++$num) {
     // Close cURL
     curl_close($ch);
 }
+    */
